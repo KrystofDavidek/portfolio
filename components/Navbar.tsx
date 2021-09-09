ffb39258f106b7651f996ac97ff40c060720577b
Krystof Davidek <kry.davidek@gmail.com>
2021-09-09 16:46:39 +0200

    initialize project

---
+++ components/Navbar.tsx
@@ -0,0 +1,45 @@
+import { FC, useEffect, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/dist/client/router";
+import { route } from "next/dist/server/router";
+
+const NavItem: FC<{ activeItem: string; setActiveItem: Function; name: string; route: string }> = ({
+  activeItem,
+  name,
+  route,
+  setActiveItem,
+}) => {
+  return activeItem !== name ? (
+    <Link href={route}>
+      <a>
+        <span onClick={() => setActiveItem(name)} className="hover:text-green">
+          {name}
+        </span>
+      </a>
+    </Link>
+  ) : null;
+};
+
+const Navbar = () => {
+  const [activeItem, setActiveItem] = useState<string>("initialState");
+  const { pathname } = useRouter();
+
+  useEffect(() => {
+    if (pathname === "/") setActiveItem("About");
+    if (pathname === "/projects") setActiveItem("Projects");
+    if (pathname === "/resume") setActiveItem("Resume");
+  }, []);
+
+  return (
+    <div className="flex justify-between px-5 py-3 my-3">
+      <span className="text-xl font-bold border-b-4 text-green md:text-2xl border-green">{activeItem}</span>
+      <div className="flex space-x-5 text-lg">
+        <NavItem activeItem={activeItem} setActiveItem={setActiveItem} name="About" route="/" />
+        <NavItem activeItem={activeItem} setActiveItem={setActiveItem} name="Projects" route="/projects" />
+        <NavItem activeItem={activeItem} setActiveItem={setActiveItem} name="Resume" route="/resume" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ components/Sidebar.tsx
@@ -0,0 +1,50 @@
+import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
+import { GoLocation } from "react-icons/go";
+import { GiTie } from "react-icons/gi";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <img src="https://picsum.photos/id/1/200/300" alt="Example img" className="w-32 h-32 mx-auto rounded-full" />
+      <h3 className="my-4 text-3xl font-medium tracking-wider font-kaushan">
+        <span className="text-green">Kryštof </span>
+        Davídek
+      </h3>
+      <p className="px-2 py-1 my-3 bg-gray-200 rounded-full">Web Developer</p>
+      <a href="" download="name" className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full">
+        <GiTie className="h-6 w-9" />
+        Download Resume
+      </a>
+      <div className="flex justify-around w-9/12 mx-auto my-5 text-green md:w-full">
+        <a href="">
+          <AiFillYoutube className="w-8 h-8 cursor-pointer" />
+        </a>
+        <a href="">
+          <AiFillGithub className="w-8 h-8 cursor-pointer" />
+        </a>
+        <a href="">
+          <AiFillLinkedin className="w-8 h-8 cursor-pointer" />
+        </a>
+      </div>
+      <div className="py-4 my-5 bg-gray-200" style={{ marginLeft: "-1rem", marginRight: "-1rem" }}>
+        <div className="flex items-center justify-center space-x-2">
+          <GoLocation />
+          <span>Brno, Czech Republic</span>
+        </div>
+        <p className="my-2">kry.davidek@gmail.com</p>
+        <p className="my-2">734574242</p>
+      </div>
+      <button
+        className="w-8/12 px-5 py-2 my-2 rounded-full bg-gradient-to-r from-green to-blue-400 focus:outline-none"
+        onClick={() => {
+          window.open("mailto:kry.davidek@gmail.com");
+        }}
+      >
+        Email Me
+      </button>
+      <button className="w-8/12 px-5 py-2 my-2 rounded-full bg-gradient-to-r from-green to-blue-400">Toggle Theme</button>
+    </div>
+  );
+};
+
+export default Sidebar;
